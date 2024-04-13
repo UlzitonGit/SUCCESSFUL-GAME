bce3f4c5af3716b38bdeb9a0ad5d29d4f9774847
Niewrsis <154369161+Niewrsis@users.noreply.github.com>
2024-04-13 17:07:35 +0300

    Reapply SoundsUI_Setting

---
+++ Assets/Scripts/Music/SoundsVolume.cs
@@ -0,0 +1,44 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundsVolume : MonoBehaviour
+{
+    #region [SerializeField]
+    /*[SerializeField] private AudioSource frogs;
+    [SerializeField] private AudioSource music;
+    [SerializeField] private AudioSource sfx;*/
+
+    [SerializeField] private TextMeshProUGUI frogsPercentText;
+    [SerializeField] private TextMeshProUGUI musicPercentText;
+    [SerializeField] private TextMeshProUGUI sfxPercentText;
+
+    [SerializeField] private Slider frogsSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    #endregion [SerializeField]
+
+    private void Awake()
+    {
+        //frogsSlider.value = frogs.volume;
+        //musicSlider.value = music.volume;
+        //sfxSlider.value = sfx.volume;
+    }
+
+    public void FrogsVolumeChange()
+    {
+        //frogs.volume = frogsSlider.value;
+        frogsPercentText.text = Mathf.RoundToInt(frogsSlider.value) + "%";
+    }
+    public void MusicVolumeChange()
+    {
+        //music.volume = musicSlider.value;
+        musicPercentText.text = Mathf.RoundToInt(musicSlider.value) + "%";
+    }
+    public void SFXVolumeChange()
+    {
+        //sfx.volume = sfxSlider.value;
+        sfxPercentText.text = Mathf.RoundToInt(sfxSlider.value) + "%";
+    }
+}
